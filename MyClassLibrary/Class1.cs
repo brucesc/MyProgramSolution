20e127074371b7a6f0314ab539e9bffe835ad4c2
Steven Bruce <Brucesc726@gmail.com>
2018-02-23 12:00:47 -0500

    initial

---
+++ MyClassLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Constants
+    {
+        public string HelloWorld = "Hello, world from the Library!";
+
+    }
+}
---
+++ MyProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibrary;
+
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Constants constants = new Constants();
+
+            Console.WriteLine(constants.HelloWorld);
+            Console.ReadKey();
+
+        }
+    }
+}
